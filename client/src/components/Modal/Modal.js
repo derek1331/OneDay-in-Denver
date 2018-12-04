b23501694492e4478b0509e975b87060651dfb39
Derek Reed <1331derek@gmail.com>
2018-12-03 21:38:06 -0700

    modal.js routes fix

--- client/src/components/Modal/Modal.js
+++ client/src/components/Modal/Modal.js
@@ -32,7 +32,7 @@ class LoginModal extends React.Component {
     console.log(this.state.username);
     //request to server here
     axios
-      .post("http://localhost:5000/api/users", {
+      .post("/api/users", {
         username: this.state.username,
         password: this.state.password
       })
@@ -76,7 +76,7 @@ class LoginModal extends React.Component {
     console.log("handleLogin");
     //request to server here
     axios
-      .post("http://localhost:5000/api/login", {
+      .post("/api/login", {
         username: this.state.username,
         password: this.state.password
       })
