4328a3164174f220ff3325c94291589ece3fe0b4
derek1331 <1331derek@gmail.com>
2019-01-07 22:17:17 -0700

    localhost

--- client/src/pages/Second/Second.js
+++ client/src/pages/Second/Second.js
@@ -36,7 +36,7 @@ class Second extends React.Component {
         // delete it from user favorites
         axios({
           method: "put",
-          url: "http://localhost:5000/api/delete",
+          url: "/api/delete",
           data: {
             username: sessionStorage.getItem("user"),
             name: event.name
@@ -45,7 +45,7 @@ class Second extends React.Component {
         // delete it from itinerary
         axios({
           method: "put",
-          url: "http://localhost:5000/api/itinerary/delete",
+          url: "/api/itinerary/delete",
           data: {
             username: sessionStorage.getItem("user"),
             id: event._id
@@ -54,7 +54,7 @@ class Second extends React.Component {
         // delete it from map
         axios({
           method: "put",
-          url: "http://localhost:5000/api/maps/delete",
+          url: "/api/maps/delete",
           data: {
             username: sessionStorage.getItem("user"),
             id: event._id
@@ -69,7 +69,7 @@ class Second extends React.Component {
         // add it to user favorites
         axios({
           method: "put",
-          url: "http://localhost:5000/api/users",
+          url: "/api/users",
           data: {
             username: sessionStorage.getItem("user"),
             name: event.name,
@@ -96,7 +96,7 @@ class Second extends React.Component {
     if (sessionStorage.getItem("user")){
     axios({
       method: "put",
-      url: "http://localhost:5000/api/favorites",
+      url: "/api/favorites",
       data: {
         username: sessionStorage.getItem("user")
       }
@@ -116,7 +116,7 @@ class Second extends React.Component {
       .then(() => {
         axios({
           method: "get",
-          url: "http://localhost:5000/api/events"
+          url: "/api/events"
         }).then(res => {
           const activity = res.data;
           this.setState({ activity });
